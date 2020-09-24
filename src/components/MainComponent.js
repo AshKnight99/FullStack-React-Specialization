6841f5fc6459b9958bea0da7d79e3861d3bb4f3e
Ashknight99 <rachanaraut1999@gmail.com>
2020-09-24 23:29:36 +0530

    React Router commit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,28 +5,35 @@ import { DISHES } from "../shared/dishes";
 import DishDetails from './DishDetailsComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch,Route,Redirect} from 'react-router-dom';
 class Main extends Component{
   constructor(props){ 
       super(props);   
     this.state = {
-      dishes: DISHES,
-      selectedDish : null
+      dishes: DISHES,     
     };
   }
-  onDishSelect(dishId) {
-    this.setState({selectedDish:dishId});
-}
+
   render(){
+      const HomePage = () => {
+          return (
+              <Home />
+          );
+      }
+      //Here we specify Menu with the parameters we need to send
     return (
       <div>
-          <Header />
-        
-        <Menu dishes={this.state.dishes}  onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetails
-         dish ={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      <Footer />
+        <Header />
+        <Switch>
+            <Route path="/home" component={HomePage} />            
+            <Route exact path="/menu" component={() =><Menu dishes={this.state.dishes} />} />
+            <Redirect to = "/home" />
+        </Switch>
+
+        <Footer />
       </div>
-      //filter function is used to searcg and give the required content in array form
+      //filter function is used to search and give the required content in array form
     );
   }
 }
